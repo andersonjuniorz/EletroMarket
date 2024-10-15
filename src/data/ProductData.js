0250bedf34d8bec3e917b4d4869ff7ec3fd16ea1
Andersonjuniorz <dev.andersonjunior@gmail.com>
2024-10-15 19:17:50 -0300

    Adicionando fotos e informacoes dos produtos

--- src/data/ProductData.js
+++ src/data/ProductData.js
@@ -1,65 +1,64 @@
 const ProductData = [
-    { 
-      "id": 1,
-      "name": "Produto A",
-      "image": "https://cdn.pixabay.com/photo/2017/04/04/17/25/sony-2202301_1280.jpg",
-      "price": 170,
-    },
-    { 
-      "id": 2,
-      "name": "Produto B",
-      "image": "https://cdn.pixabay.com/photo/2017/04/04/17/25/sony-2202301_1280.jpg",
-      "price": 170,
+  { 
+    "id": 1,
+    "name": "Placa de Vídeo RTX 4060 VENTUS 2x, 8GB GDDR6",
+    "image": "https://images2.kabum.com.br/produtos/fotos/469132/placa-de-video-rtx-4060-ventus-2x-black-oc-msi-nvidia-geforce-8gb-gddr6-dlss-ray-tracing_1688052210_g.jpg",
+    "price": 1945.99,
+  },
+  { 
+    "id": 2,
+    "name": "Monitor Gamer LG 29 LED, Ultra Wide FUll HD, HDMI",
+    "image": "https://images4.kabum.com.br/produtos/fotos/90224/monitor-gamer-lg-29-ips-ultra-wide-75-hz-full-hd-freesync-99-srgb-hdmi-displayport-vesa-29um69g-b_1715693876_g.jpg",
+    "price": 989.89,
+  },
+  { 
+    "id": 3,
+    "name": "Notebook Gamer Acer Nitro V15, intel i7, 16GB",
+    "image": "https://images5.kabum.com.br/produtos/fotos/617805/notebook-gamer-acer-nitro-v15-intel-core-i7-13620h-16gb-ram-rtx-3050-ssd-512gb-15-6-full-hd-ips-144hz-linux-preto-anv15-51-73e9_1722888806_g.jpg",
+    "price": 5449.99,
+  },
+  { 
+    "id": 4,
+    "name": "Console PlayStation 5 Pro, 2TB",
+    "image": "https://images0.kabum.com.br/produtos/fotos/636960/console-playstation-5-pro-sony-ssd-2tb-controle-sem-fio-dualsense-branco-1000046552_1727095996_g.jpg",
+    "price": 6509.07,
+  },
+  { 
+    "id": 5,
+    "name": "Jogo God of War Ragnarök para PS5",
+    "image": "https://images2.kabum.com.br/produtos/fotos/396722/jogo-god-of-war-ragnar-k-edicao-standard-ps5-_1666892140_g.jpg",
+    "price": 269.10,
+  },
+  { 
+    "id": 6,
+    "name": "Memória RAM Kingston Fury RGB, 32GB",
+    "image": "https://images9.kabum.com.br/produtos/fotos/448299/memoria-kingston-fury-beast-rgb-32gb-5200mhz-ddr5-cl36-branco-kf552c36bwea-32_1682624571_g.jpg",
+    "price": 839.99,
+  },
+  { 
+    "id": 7,
+    "name": "Console Microsoft Xbox Series S, 512GB",
+    "image": "https://images5.kabum.com.br/produtos/fotos/sync_mirakl/588935/Console-Microsoft-Xbox-Series-S-512GB-Controle-e-Cabos_1722357646_g.jpg",
+    "price": 2939.00,
+  },
+  { 
+    "id": 8,
+    "name": "Monitor Gamer Curvo 27'' 100Hz, HDMI - VGA",
+    "image": "https://images6.kabum.com.br/produtos/fotos/sync_mirakl/504136/Monitor-Gamer-Curvo-Conc-rdia-27-100Hz-Led-FULL-HD-HDMI-VGA-Cz270f_1725975560_g.jpg",
+    "price": 750.55,
+  },
+  { 
+    "id": 9,
+    "name": "Water Cooler Rise Mode, 240mm ARGB",
+    "image": "https://images5.kabum.com.br/produtos/fotos/248205/water-cooler-gamer-rise-mode-argb-intel-e-amd-240mm-preto-rm-wcb-04-argb_1663778106_g.jpg",
+    "price": 199.99,
+  },
+  { 
+    "id": 10,
+    "name": "PC Gamer Aquario, Ryzen 7, RTX 3060",
+    "image": "https://images4.kabum.com.br/produtos/fotos/sync_mirakl/633184/PC-Gamer-Aquario-Preto-Ryzen-7-5700x-RTX-3060-32gb-SSD-1TB-Water-Cooler-M-ximo-Desempenho-_1725480945_g.jpg",
+    "price": 6174.00,
+  }
+];
 
-    },
-    { 
-      "id": 3,
-      "name": "Produto C",
-      "image": "https://cdn.pixabay.com/photo/2017/04/04/17/25/sony-2202301_1280.jpg",
-      "price": 170,
-    },
-    { 
-      "id": 4,
-      "name": "Produto D",
-      "image": "https://cdn.pixabay.com/photo/2017/04/04/17/25/sony-2202301_1280.jpg",
-      "price": 170,
-    },
-    { 
-      "id": 5,
-      "name": "Produto A",
-      "image": "https://cdn.pixabay.com/photo/2017/04/04/17/25/sony-2202301_1280.jpg",
-      "price": 170,
-    },
-    { 
-      "id": 6,
-      "name": "Produto B",
-      "image": "https://cdn.pixabay.com/photo/2017/04/04/17/25/sony-2202301_1280.jpg",
-      "price": 170,
-    },
-    { 
-      "id": 7,
-      "name": "Produto C",
-      "image": "https://cdn.pixabay.com/photo/2017/04/04/17/25/sony-2202301_1280.jpg",
-      "price": 170,
-    },
-    { 
-      "id": 8,
-      "name": "Produto D",
-      "image": "https://cdn.pixabay.com/photo/2017/04/04/17/25/sony-2202301_1280.jpg",
-      "price": 170,
-    },
-    { 
-      "id": 9,
-      "name": "Produto C",
-      "image": "https://cdn.pixabay.com/photo/2017/04/04/17/25/sony-2202301_1280.jpg",
-      "price": 170,
-    },
-    { 
-      "id": 10,
-      "name": "Produto D",
-      "image": "https://cdn.pixabay.com/photo/2017/04/04/17/25/sony-2202301_1280.jpg",
-      "price": 170,
-    }
-  ];
-
-  export default ProductData;
+export default ProductData;
